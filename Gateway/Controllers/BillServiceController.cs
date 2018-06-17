73892c289d8c7fdb7c4f4847766164681b7d404a
Chetvertiy <romzet95@mail.ru>
2018-06-17 20:26:31 +0300

    Added a gateway, controllers for every microservice. Not implemented fully.

---
+++ Gateway/Controllers/BillServiceController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BillService;
+using BillService.Models;
+
+namespace Gateway.Controllers
+{
+    public class BillServiceController : ApiController
+    {
+        // GET api/billservice
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/billservice/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/billservice
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/billservice/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/billservice/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ Gateway/Controllers/FeedbackServiceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using FeedbackService;
+using FeedbackService.Models;
+
+namespace Gateway.Controllers
+{
+    public class FeedbackServiceController : ApiController
+    {
+        // GET api/feedbackservice
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/feedbackservice/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/feedbackservice
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // DELETE api/feedbackservice/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
